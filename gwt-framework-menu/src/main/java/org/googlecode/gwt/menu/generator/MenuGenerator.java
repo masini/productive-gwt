3399ff20d29872e3aadef9a18a432e1db0295383
marco.gervasoni <marco.gervasoni@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2008-01-16 13:56:55 +0000

    tolto parametro errato

--- gwt-framework-menu/src/main/java/org/googlecode/gwt/menu/generator/MenuGenerator.java
+++ gwt-framework-menu/src/main/java/org/googlecode/gwt/menu/generator/MenuGenerator.java
@@ -1,6 +1,5 @@
 package org.googlecode.gwt.menu.generator;
 
-import com.google.gwt.core.ext.BadPropertyValueException;
 import com.google.gwt.core.ext.Generator;
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
@@ -9,23 +8,10 @@ import com.google.gwt.core.ext.typeinfo.NotFoundException;
 
 public class MenuGenerator extends Generator {
 
-	private static final String MENU_LOCATION = "org.googlecode.Menu";
-	
 	public String generate(TreeLogger logger, GeneratorContext context, String typeName) throws UnableToCompleteException {
 		try {
 			ClassMenuGenerator classMenuBar = new ClassMenuGenerator(logger, context);
-
-			String location = context.getPropertyOracle().getPropertyValue(logger, MENU_LOCATION);
-			location = location.replaceAll("_", ".");
-			
-			if(location!=null && !location.equals("")){
-				return classMenuBar.create(location);
-			}
-			else{
-				String msg = "Property  '" + MENU_LOCATION + "' find without value.";
-				logger.log(TreeLogger.ERROR, msg, new MenuGeneratorException(msg));
-				return "";
-			}
+			return classMenuBar.create(typeName);
 
 		} catch (ClassNotFoundException e) {
 			logger.log(TreeLogger.ERROR, "Reflaction java: Class '" + typeName + "' Not Found", e);
@@ -42,11 +28,6 @@ public class MenuGenerator extends Generator {
 		} catch (IllegalArgumentException e) {
 			logger.log(TreeLogger.ERROR, "Class '" + typeName + "' Not Found", e);
 			throw new UnableToCompleteException();
-		} catch (BadPropertyValueException e) {
-			logger.log(TreeLogger.ERROR, "Property  '" + MENU_LOCATION + "' Not Found", e);
-			throw new UnableToCompleteException();
-		}
-
+		} 
 	}
-
 }
