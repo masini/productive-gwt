fbd5e9ce081f518b4d969bc0b46fb4b3c58f8096
todeschini.andrea <todeschini.andrea@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2009-03-27 13:39:51 +0000

    ultima versione prima delle modifiche per framework pda

--- gwt-framework-bootstrap/src/main/java/org/googlecode/gwt/bootstrap/client/utils/BootstrapConstants.java
+++
@@ -1,9 +0,0 @@
-package org.googlecode.gwt.bootstrap.client.utils;
-
-import com.google.gwt.i18n.client.Constants;
-
-public interface BootstrapConstants extends Constants {
-			
-	String errore_title();
-
-}
--- gwt-framework-menu/src/main/java/org/googlecode/gwt/menu/client/SMenu.java
+++
@@ -1,9 +0,0 @@
-package org.googlecode.gwt.menu.client;
-
-import org.googlecode.gwt.menu.client.model.MenuModel;
-
-public interface SMenu {
-	
-
-	MenuModel getMenuModel();
-}
