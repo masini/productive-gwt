0d4d88abe45a56fd01b096b0537bf8b285c46c67
grazia.marchione <grazia.marchione@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2009-06-30 09:08:48 +0000

    Inseriti test add e remove ValueChangeHandler

--- gwt-framework-reflection/src/test/java/org/googlecode/gwt/reflection/client/ReflectionTestNewOwnPojo.java
+++ gwt-framework-reflection/src/test/java/org/googlecode/gwt/reflection/client/ReflectionTestNewOwnPojo.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.junit.client.GWTTestCase;
 
 public class ReflectionTestNewOwnPojo extends GWTTestCase {
@@ -13,6 +15,11 @@ public class ReflectionTestNewOwnPojo extends GWTTestCase {
 	private static final String TEST = "Test";
 	private static WrapperFactory<NewOwnPojo> factory;
 	private static WrapperFactory.Wrapper<NewOwnPojo> wrapper;
+	private static ValueChangeHandler<String> valueChangeHandler;
+	private static ValueChangeHandler<String> valueChangeHandlerProperty;
+	private static boolean changedProperty = false;
+	private static boolean changed = false;
+	
 	private static String PI = "3.14159265358979323846";
 	
 	public static void testCreateWrapperFactory() {
@@ -35,6 +42,8 @@ public class ReflectionTestNewOwnPojo extends GWTTestCase {
 		assertNotNull("Wrapper non generato", wrapper);
 	}
 	
+	
+	
 	public static void testNoProperty() {
 		
 		boolean exception = false;
@@ -55,7 +64,7 @@ public class ReflectionTestNewOwnPojo extends GWTTestCase {
 		
 		assertNotNull("listField null !!", property);
 		
-		assertEquals("La stringa ritornata non è corretta", TEST,property.get(0));
+		assertEquals("La stringa ritornata non ï¿½ corretta", TEST,property.get(0));
 	}
 	
 	public static void testBigDecimalProperty() {
@@ -64,7 +73,65 @@ public class ReflectionTestNewOwnPojo extends GWTTestCase {
 		
 		assertNotNull("bigDecimal null !!", property);
 		
-		assertEquals("La stringa ritornata non è corretta", new BigDecimal(PI), property);
+		assertEquals("La stringa ritornata non ï¿½ corretta", new BigDecimal(PI), property);
+	}
+	
+	public static void testAddValueChangeListener() {
+		changed = false;
+		valueChangeHandler = new ValueChangeHandler<String>() {
+			
+			public void onValueChange(ValueChangeEvent<String> event) {
+				changed = true;
+			}
+		};
+		
+		wrapper.addValueChangeHandler(valueChangeHandler);
+		
+		wrapper.setPropertyAsString("longField", "001");
+		
+		assertTrue("Non rileva modifiche ad una property del pojo", changed);
+		
+	}
+	
+	public static void testRemoveValueChangeListener() {
+		changed = false;
+				
+		wrapper.removeValueChangeHandler(valueChangeHandler);
+		
+		wrapper.setPropertyAsString("longField", "001");
+		
+		assertFalse("Rileva modifiche ad una property del pojo avendo rimosso l'handler", changed);
+		
+	}
+
+	
+	public static void testAddValueChangeListenerProperty() {
+		changedProperty = false;
+		
+		valueChangeHandlerProperty = new ValueChangeHandler<String>() {
+			
+			public void onValueChange(ValueChangeEvent<String> event) {
+				changedProperty = true;
+			}
+		};
+		
+		wrapper.addValueChangeHandler("longField", valueChangeHandlerProperty);
+		
+		wrapper.setPropertyAsString("longField", "001");
+		
+		assertTrue("Non rileva modifiche alla property longField del pojo", changedProperty);
+		
+		
+	}
+	public static void testRemoveValueChangeHandlerProperty() {
+		changedProperty = false;
+		
+		wrapper.removeValueChangeHandler("longField", valueChangeHandlerProperty);
+		
+		wrapper.setPropertyAsString("longField", "001");
+		
+		assertFalse("Rileva modifiche alla property longField del pojo avendo rimosso l'handler", changedProperty);
+		
 	}
 	
 	@Override
