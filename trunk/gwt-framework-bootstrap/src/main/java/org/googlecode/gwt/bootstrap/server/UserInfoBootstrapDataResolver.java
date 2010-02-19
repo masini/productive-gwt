0ae434bd44577323729854dfbe010522acdcf89d
marco.gervasoni <marco.gervasoni@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2010-02-19 16:33:57 +0000

    gestione paramentri mancanti rispetto a vecchio spring-web

---
+++ trunk/gwt-framework-bootstrap/src/main/java/org/googlecode/gwt/bootstrap/server/UserInfoBootstrapDataResolver.java
@@ -0,0 +1,6 @@
+package org.googlecode.gwt.bootstrap.server;
+
+public interface UserInfoBootstrapDataResolver extends BootstrapDataResolver {
+
+	public void setUserInfoResolver( UserInfoResolver userInfoResolver );
+}
