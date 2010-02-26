a28a8eea1fc5377e60e46b250459152d83c99280
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2010-02-26 13:39:07 +0000

    Corretti bachetti vari

--- gwt-framework-footer/src/main/java/org/googlecode/gwt/footer/client/Footer.java
+++ gwt-framework-footer/src/main/java/org/googlecode/gwt/footer/client/Footer.java
@@ -1,5 +1,7 @@
 package org.googlecode.gwt.footer.client;
 
+import java.util.Date;
+
 import org.googlecode.gwt.template.client.TemplateManager;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -11,6 +13,12 @@ import com.google.gwt.user.client.ui.Label;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Footer implements EntryPoint {
+	
+	@SuppressWarnings({ "deprecation" })
+	private int getYear() {
+		return new Date().getYear()+1900;
+	}
+	
 	public void onModuleLoad() {
 		
 		AbsolutePanel absolutePanel = new AbsolutePanel();
@@ -18,8 +26,7 @@ public class Footer implements EntryPoint {
 		 
 		absolutePanel.setSize("100%", "100%");
 		
-		
-		Label label = new Label("sdf asdf -sd f-sdf- sd-fs- f- sd");
+		Label label = new Label("Esselunga (c) "+getYear());
 		label.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 		
 		absolutePanel.add(label);
