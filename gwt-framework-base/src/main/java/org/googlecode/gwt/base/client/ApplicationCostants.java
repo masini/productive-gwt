682ad0b9e2a6d898bd860a3de79608edca3d7a92
davide.ling77 <davide.ling77@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2010-04-20 10:07:18 +0000

    Moved header costants to base

--- gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ApplicationCostants.java
+++ gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ApplicationCostants.java
@@ -5,19 +5,19 @@ import com.google.gwt.i18n.client.Constants;
 public interface ApplicationCostants  extends Constants {
 
 	/**
-	 * Label della intranet 
+	 * Intranet Label 
 	 */
-	@DefaultStringValue("Torna alla Intranet")
+	@DefaultStringValue("Back to Intranet")
 	String INTRANET_PAGE_CONTEXT_LABEL();
 	
 	/**
-	 * Label della homepage
+	 * Homepage Label
 	 */
 	@DefaultStringValue("Home")
 	String HOME_PAGE_CONTEXT_LABEL();
 
 	/**
-	 * Separatore del path
+	 * Path separator
 	 */
 	@DefaultStringValue("&nbsp;&raquo;&nbsp;")
 	String PAGE_CONTEXT_SEPARATOR();
@@ -30,5 +30,35 @@ public interface ApplicationCostants  extends Constants {
 	
 	@DefaultStringValue("My Copyright (c)")
 	String COPYRIGHT();
+	
+	@DefaultStringValue("Welcome")
+	String WELCOME_LABEL();
+	
+	@DefaultStringValue("User")
+	String CURRENT_USER_NAME_LABEL();
+	
+	@DefaultStringValue("Userid")
+	String CURRENT_USER_ID_LABEL();
+	
+	@DefaultStringValue("Cluster name")
+	String CLUSTER_NAME_LABEL();
+	
+	@DefaultStringValue("Server name")
+	String SERVER_NAME_LABEL();
+	
+	@DefaultStringValue("Application name")
+	String APPLICATION_NAME_LABEL();
+	
+	@DefaultStringValue("Application code")
+	String APPLICATION_CODE_LABEL();
+	
+	@DefaultStringValue("Application version")
+	String APPLICATION_VERSION_LABEL();
+	
+	@DefaultStringValue("Roles")
+	String ROLES_LABEL();
+	
+	@DefaultStringValue("Parameters")
+	String PARAMETERS_LABEL();
 	
 }
