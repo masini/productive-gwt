c7033bd3aeafd6a612b52357a51d6cf349017a09
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2011-03-18 14:04:37 +0000

    Modifiche per prendere nome del cluster da JMX

--- trunk/gwt-framework/gwt-framework-bootstrap/src/main/java/org/googlecode/gwt/bootstrap/server/jmx/JMXServerState.java
+++ trunk/gwt-framework/gwt-framework-bootstrap/src/main/java/org/googlecode/gwt/bootstrap/server/jmx/JMXServerState.java
@@ -10,24 +10,35 @@ public class JMXServerState {
 	
     private final static String RUNTIMESERVICE = "com.bea:Name=RuntimeService,Type=weblogic.management.mbeanservers.runtime.RuntimeServiceMBean";
 	
-	public static Object getServerConfigurationAttribute(String attribute) throws AttributeNotFoundException {
-		try {
-			InitialContext ctx = new InitialContext();
-			MBeanServer server = (MBeanServer) ctx.lookup("java:comp/env/jmx/runtime");
-			
-			ObjectName serverConfiguration = (ObjectName) server.getAttribute(
-					new ObjectName(
-							RUNTIMESERVICE), "ServerConfiguration");
+    public static MBeanServer getMBeanServer() {
+        try {
+            InitialContext ctx = new InitialContext();
+            MBeanServer server = (MBeanServer) ctx.lookup("java:comp/env/jmx/runtime");
+            ctx.close();
+            return server;
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 
-			Object name = server.getAttribute(serverConfiguration, attribute);
-			return name;
-		} catch (AttributeNotFoundException e) {
-			throw e;
-		}
-		catch(Exception e){
-			throw new RuntimeException(e);
-		}
-	}  	  	  	    
+    public static Object getServerConfigurationAttribute(String...paths) {
+        try {
+            MBeanServer server = getMBeanServer();
+
+            Object currentPath = new ObjectName(RUNTIMESERVICE);
+            for(String nextPath: paths) {
+                if( currentPath==null ) {
+                    break;
+                }
+                currentPath = server.getAttribute((ObjectName) currentPath, nextPath);
+            }
+
+            return currentPath;
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 
 	public static String getServerConfigurationAttributeAsString(final String attribute, final String defaultValue) {
 		
