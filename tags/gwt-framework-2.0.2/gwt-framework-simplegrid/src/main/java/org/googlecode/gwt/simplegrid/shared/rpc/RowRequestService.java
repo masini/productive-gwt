f0a0d086dcc9b85c88d314b53509fe361c3b86d7
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2011-03-29 15:09:08 +0000

    errore

--- tags/gwt-framework-2.0.2/gwt-framework-simplegrid/src/main/java/org/googlecode/gwt/simplegrid/shared/rpc/RowRequestService.java
+++
@@ -1,9 +0,0 @@
-package org.googlecode.gwt.simplegrid.shared.rpc;
-
-import java.io.Serializable;
-
-import org.googlecode.gwt.simplegrid.shared.DataResponse;
-
-public interface RowRequestService<ROW extends Serializable, FILTER extends Serializable> extends Service {
-	DataResponse<ROW> requestRows(StandardDataRequest<FILTER> request) throws Exception;
-}
