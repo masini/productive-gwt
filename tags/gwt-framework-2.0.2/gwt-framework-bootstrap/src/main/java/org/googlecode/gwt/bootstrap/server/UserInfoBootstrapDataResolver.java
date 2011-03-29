004717dba077a281bc9cac9ae08215e2f704ccda
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2011-03-29 10:27:06 +0000

    errore

--- tags/gwt-framework-2.0.2/gwt-framework-bootstrap/src/main/java/org/googlecode/gwt/bootstrap/server/UserInfoBootstrapDataResolver.java
+++
@@ -1,6 +0,0 @@
-package org.googlecode.gwt.bootstrap.server;
-
-public interface UserInfoBootstrapDataResolver extends BootstrapDataResolver {
-
-	public void setUserInfoResolver( UserInfoResolver userInfoResolver );
-}
