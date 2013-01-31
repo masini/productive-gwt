0e6f6d120210cce50ed035256051b4cb530e9a1e
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2013-01-31 13:26:32 +0000

    da rifare

--- tags/gwt-framework-2.0.3/gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ExtendedUserInfo.java
+++
@@ -1,7 +0,0 @@
-package org.googlecode.gwt.base.client;
-
-public interface ExtendedUserInfo extends UserInfo {
-    boolean isUserInRole(String role);
-    String getUserParameter( String parameterName );
-    String getRoleDescription( String role );
-}
