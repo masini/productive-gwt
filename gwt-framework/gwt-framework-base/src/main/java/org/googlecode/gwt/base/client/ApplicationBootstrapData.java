8cf6aa65671382bf126966e1e83ac983a61b1504
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2013-02-15 09:29:16 +0000

    Disabilitata serializzazione di default perché quando il bean è decorato con un proxy vengono serializzati anche campi non applicativi

--- gwt-framework/gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ApplicationBootstrapData.java
+++ gwt-framework/gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ApplicationBootstrapData.java
@@ -1,10 +1,15 @@
 package org.googlecode.gwt.base.client;
 
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import java.io.Serializable;
 import java.util.Map;
 
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
 public interface ApplicationBootstrapData  extends Serializable {
 
+    @JsonProperty
     Map<String, String> getApplicationData();
 
 }
