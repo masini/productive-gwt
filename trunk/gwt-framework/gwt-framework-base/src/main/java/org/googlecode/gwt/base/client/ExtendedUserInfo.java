ef8621317bfcbd826bbfec1079c33dbaaf414805
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2013-02-15 13:53:29 +0000

    NoCaching su Base e Bootstrap

--- trunk/gwt-framework/gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ExtendedUserInfo.java
+++ trunk/gwt-framework/gwt-framework-base/src/main/java/org/googlecode/gwt/base/client/ExtendedUserInfo.java
@@ -1,5 +1,8 @@
 package org.googlecode.gwt.base.client;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public interface ExtendedUserInfo extends UserInfo {
     boolean isUserInRole(String role);
     String getUserParameter( String parameterName );
