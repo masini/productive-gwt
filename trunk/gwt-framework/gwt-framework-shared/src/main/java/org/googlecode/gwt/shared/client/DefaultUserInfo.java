7865b574e876be912238d020e0895585562e6200
luca.masini <luca.masini@0ae46c31-5d43-0410-b0ff-25ec9014f3a9>
2014-01-15 09:53:23 +0000

    aggiunta info sul codaz nel bootstrap

--- trunk/gwt-framework/gwt-framework-shared/src/main/java/org/googlecode/gwt/shared/client/DefaultUserInfo.java
+++ trunk/gwt-framework/gwt-framework-shared/src/main/java/org/googlecode/gwt/shared/client/DefaultUserInfo.java
@@ -16,16 +16,20 @@ public class DefaultUserInfo implements UserInfo {
     private String username;
     private String firstName;
     private String lastName;
+    private String codaz;
+    private String soc;
     private HashMap<String,String> roleDescriptions = new HashMap<String,String>();
     private HashMap<String,String> userParameters = new HashMap<String,String>();
 
     private static final long serialVersionUID = 1931201066080419627L;
 
-    public DefaultUserInfo( String username, String firstName, String lastName, List<String> roles, Map<String,String> userParameters ) {
+    public DefaultUserInfo( String username, String firstName, String lastName, String codaz, String soc, List<String> roles, Map<String,String> userParameters) {
         this.username = username;
         this.userParameters = checkNullMap(userParameters);
         this.firstName = firstName;
         this.lastName = lastName;
+        this.codaz = codaz;
+        this.soc = soc;
         this.roleDescriptions = new HashMap<String,String>();
         if(roles != null) {
             for(String role: roles) {
@@ -36,17 +40,20 @@ public class DefaultUserInfo implements UserInfo {
 
     public DefaultUserInfo(UserInfo userInfo) {
         this(userInfo.getUsername(), userInfo.getFirstName(),
-             userInfo.getLastName(), userInfo.getRoles(), userInfo.getUserParameters());
+                userInfo.getLastName(),  userInfo.getCodaz(), userInfo.getSoc(),
+                userInfo.getRoles(), userInfo.getUserParameters());
     }
 
-	private HashMap<String, String> checkNullMap(Map<String, String> map) {
-		return (map == null ? new HashMap<String,String>() : new HashMap<String,String>(map));
-	}
+    private HashMap<String, String> checkNullMap(Map<String, String> map) {
+        return (map == null ? new HashMap<String,String>() : new HashMap<String,String>(map));
+    }
 
-    public DefaultUserInfo( String username, String firstName, String lastName, Map<String,String> roleDescriptions, Map<String,String> userParameters ) {
+    public DefaultUserInfo( String username, String firstName, String lastName, String codaz, String soc, Map<String,String> roleDescriptions, Map<String,String> userParameters ) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.codaz = codaz;
+        this.soc = soc;
         this.roleDescriptions = checkNullMap(roleDescriptions);
         this.userParameters = checkNullMap(userParameters);
     }
@@ -78,9 +85,9 @@ public class DefaultUserInfo implements UserInfo {
     public String getUserParameter( String parameterName ) {
         return ( String )userParameters.get( parameterName );
     }
-    
+
     public List<String> getUserParameterNames() {
-    	return new ArrayList<String>(userParameters.keySet());
+        return new ArrayList<String>(userParameters.keySet());
     }
 
     public void setUsername( String username ) {
@@ -120,5 +127,14 @@ public class DefaultUserInfo implements UserInfo {
 
     public HashMap<String,String> getUserParameters() {
         return userParameters;
+    }
+
+    public String getCodaz() {
+        return codaz;
+    }
+
+    @Override
+    public String getSoc() {
+        return soc;
     }
 }
